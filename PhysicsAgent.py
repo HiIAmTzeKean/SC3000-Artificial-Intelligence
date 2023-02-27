d63ae76de984cb39fe509e873c185632b15f41e7
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-02-27 22:10:20 +0800

    feat: decay and packaging for QL agent

--- PhysicsAgent.py
+++ PhysicsAgent.py
@@ -1,4 +1,4 @@
-from rlagent import RLAgent, CartpoleWorld, Observation
+from qlagent import RLAgent, CartpoleWorld, Observation
 
 class PhysicsAgent(RLAgent):
     
@@ -15,17 +15,11 @@ class PhysicsAgent(RLAgent):
             self.move(s)
         return self._total_reward
     
-    def get_optimal_action(self, s: Observation):
-        """Reference: https://towardsdatascience.com/how-to-beat-the-cartpole-game-in-5-lines-5ab4e738c93f
-
-        Args:
-            s (Observation): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        #return 1 if s[2] > 0 else 0
     
+    def get_optimal_action(self, s: Observation) -> int:
+        """Reference: https://towardsdatascience.com/how-to-beat-the-cartpole-game-in-5-lines-5ab4e738c93f
+            Overrides abstract base class
+        """
         theta, w = s[2:4]
         if abs(theta) < 0.03:
             return 0 if w < 0 else 1
--- qlagent.py
+++ qlagent.py
@@ -91,6 +91,14 @@ class RLAgent(ABC):
         pass
     
     def wrap_observation(self, observation: np.ndarray) -> Observation:
+        """Converts numpy array to Observation object
+
+        Args:
+            observation (np.ndarray): array to pass in from cartpole
+
+        Returns:
+            Observation: Object to return
+        """
         return Observation(*observation)
     
     def discretise_observation(self, observation: np.ndarray) -> Observation:
@@ -107,13 +115,24 @@ class RLAgent(ABC):
 class QLearningAgent(RLAgent):
     def __init__(self, env:CartpoleWorld) -> None:
         super().__init__(env)
-        self.__learning_rate = 0.2
+        
+        # defines learning rate
+        self.__learning_rate = 0.5
+        self.__learning_rate_min = 0.2
+        
         # defined for epsilon soft policy
-        self.__epsilon = 0.1
+        # initially set to a large number to encourage exploration
+        # epsilon will decay as episodes increase
+        self.__epsilon = 0.9
+        self._epsilon_min = 0.1
+        
         # dictionary of (state,action) -> quality
         self.__q_table : Dict[Tuple[Observation,int],float] = dict()
         self.__pi_table : Dict[Observation, int] = dict()
-        self.load_pickle('qLearningAgent_pi.pkl','qLearningAgent_q.pkl')
+        
+        # Load pickle file to restore agent previous state
+        self.load_pickle('QL_parameters.pkl')
+        
         # [left, right] action set
         self.__actions = [0,1]
         self.__discounted_reward = 0.9
@@ -132,14 +151,33 @@ class QLearningAgent(RLAgent):
             self.__pi_table[s] = self.get_random_action()
         return self.__pi_table[s]
     
+    def update_parameters(self) -> None:
+        self.decay_epsilon()
+        self.decay_learning_rate()
+    
+    def decay_epsilon(self) -> None:
+        if self.__epsilon <= self._epsilon_min:
+            return
+        self.__epsilon *= 0.999999
+    
+    def decay_learning_rate(self) -> None:
+        if self.__learning_rate <= self.__learning_rate_min:
+            return
+        self.__learning_rate *= 0.9999999
+    
     def run(self,  num_of_episode: int):
+        """Overrides base class method
+
+        Args:
+            num_of_episode (int): Number of episdoe to run
+        """
         cumulated_reward = 0
-        for i in range(num_of_episode):
+        for _ in range(num_of_episode):
             cumulated_reward += self.run_single_episode()
         
-        print(f"Epsilon: {self.__epsilon}, Discounted reward: {self.__discounted_reward}")
+        print(f"Epsilon: {self.__epsilon}, Discounted reward: {self.__discounted_reward}, Learning rate: {self.__learning_rate}")
         print(f"Mean reward is: {cumulated_reward/num_of_episode} for {num_of_episode} episodes")
-        self.save_pickle('qLearningAgent_pi.pkl','qLearningAgent_q.pkl')
+        self.save_pickle("QL_parameters.pkl")
 
     def run_single_episode(self) -> int:
         # clear history
@@ -156,7 +194,7 @@ class QLearningAgent(RLAgent):
             s_prime = self.discretise_observation(s_prime)
             
             self.update_q_table(s,R,s_prime)
-        # print(f"Episode completed: reward {self._total_reward}")
+            self.update_parameters()
         return self._total_reward
 
     def update_q_table(self,s: Observation, R: float, s_prime: Observation):
@@ -206,50 +244,32 @@ class QLearningAgent(RLAgent):
     
     def get_pi_table(self):
         return self.__pi_table
-    
-    def load_tables(self,pi_table,q_table):
-        """Depreciated.
-        Sets the agent table to args.
-
-        Args:
-            pi_table (Dict): Dictionary of (state,action)
-            q_table (Dict): Dictionary of pi for each state
-        """
-        self.__pi_table = pi_table
-        self.__q_table = q_table
         
-    def load_pickle(self,pi_table_file: str, q_table_file: str):
+    def load_pickle(self, parameters_file: str):
         """Loads pickle file to agent table
 
         Args:
-            pi_table_file (str): pickle file location
-            q_table_file (str): pickle file location
+            parameters (str): pickle file location
         """
-        if (os.path.exists('qLearningAgent_pi.pkl') and os.path.exists('qLearningAgent_pi.pkl')):
-            # load pickle
-            with open(pi_table_file, 'rb') as file:
+        if os.path.exists(parameters_file):
+            with open(parameters_file, 'rb') as file:
                 # Call load method to deserialze
-                self.__pi_table = pickle.load(file)
-            
-            with open(q_table_file, 'rb') as file:
-                # Call load method to deserialze
-                self.__q_table = pickle.load(file)
+                self.__pi_table,self.__q_table,self.__epsilon,self.__learning_rate = pickle.load(file)
         else:
+            print("*** LOG: Pickle file not found")
             pass
         
-    def save_pickle(self,pi_table_file: str, q_table_file: str):
+    def save_pickle(self, parameters_file: str):
         """Saves q and pi table to pickle.
 
         Args:
             pi_table_file (str): location of file
             q_table_file (str): location of file
         """
-        with open(pi_table_file, 'wb') as file:
-            # A new file will be created
-            pickle.dump(agent.get_pi_table(), file)
-        with open(q_table_file, 'wb') as file:
-            # A new file will be created
-            pickle.dump(agent.get_q_table(), file)
+        with open(parameters_file, 'wb') as file:
+            # Call load method to deserialze
+            pickle.dump([self.__pi_table,self.__q_table,self.__epsilon,self.__learning_rate], file)
+        
     
 import os
 if __name__ == "__main__":
