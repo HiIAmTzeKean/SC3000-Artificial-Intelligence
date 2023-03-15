7f173c103eae6d7e523585ae85cb9db039a8a513
Ng Tze Kean <ngtzekean600@gmail.com>
2023-03-16 00:20:49 +0800

    feat: new parms in Q agent
    the agent is performing worst off now at 200 only. Unable to find the
    reason why. Need to tune the old parameters to find out more.

--- qlagent.py
+++ qlagent.py
@@ -116,27 +116,40 @@ class RLAgent(ABC):
         observation[2] = round(observation[2],2)
         observation[3] = round(observation[3],0)
         return Observation(*observation)
-    
+
+# define constants
+MULTIPLICATIVE_DECAY=0
+
 class QLearningAgent(RLAgent):
-    def __init__(self, env:CartpoleWorld) -> None:
+    """Q-learning agent
+
+    Args:
+        RLAgent (class): Abstract base class
+    """
+    def __init__(self, env:CartpoleWorld, to_load_pickle=True, to_save_pickle=True, max_epoch=100000, epsilon_decay=MULTIPLICATIVE_DECAY) -> None:
         super().__init__(env)
         
         # defines learning rate
         self.__learning_rate = 0.5
-        self.__learning_rate_min = 0.2
+        self.__learning_rate_min = 0.01
         
         # defined for epsilon soft policy
         # initially set to a large number to encourage exploration
         # epsilon will decay as episodes increase
+        self.__epsilon_decay = epsilon_decay
+        self.__num_epoch = 0
+        self.__max_epoch = max_epoch
         self.__epsilon = 0.9
-        self._epsilon_min = 0.1
+        self._epsilon_min = 0.01
         
         # dictionary of (state,action) -> quality
         self.__q_table : Dict[Tuple[Observation,int],float] = dict()
         self.__pi_table : Dict[Observation, int] = dict()
         
         # Load pickle file to restore agent previous state
-        self.load_pickle('QL_parameters.pkl')
+        if (to_load_pickle):
+            self.load_pickle('QL_parameters.pkl')
+        self.__to_save_pickle = to_save_pickle
         
         # [left, right] action set
         self.__actions = [0,1]
@@ -146,24 +159,6 @@ class QLearningAgent(RLAgent):
         self.__is_production = False
         
     
-    def get_optimal_action(self, s: Observation):
-        # a* is the argmax_a Q(s,a)
-        a_star: int = self.argmax_a_Q(s,self.__actions)
-        
-        if (self.__is_production):
-            return a_star
-        
-        epsilon_over_A: float = self.__epsilon / len(self.__actions)
-        
-        # apply epsilon soft policy here to encourage exploration
-        if (np.random.randn() < 1 - self.__epsilon + epsilon_over_A):
-            # pick optimal
-            self.__pi_table[s] = a_star
-        else:
-            # pick random
-            self.__pi_table[s] = self.get_random_action()
-        return self.__pi_table[s]
-    
     def update_parameters(self) -> None:
         self.decay_epsilon()
         self.decay_learning_rate()
@@ -171,26 +166,47 @@ class QLearningAgent(RLAgent):
     def decay_epsilon(self) -> None:
         if self.__epsilon <= self._epsilon_min:
             return
-        self.__epsilon *= 0.999999
+        
+        self.__num_epoch +=1
+        if (self.__epsilon_decay==MULTIPLICATIVE_DECAY):
+            # multiplicative decrease
+            self.__epsilon *= 0.999999
+        else:
+            # exponential decrease
+            # hyperparamters to tune
+            A=0.5
+            B=0.1
+            C=0.1
+            standardized_time=(self.__num_epoch-A*self.__max_epoch)/(B*self.__max_epoch)
+            cosh=np.cosh(math.exp(-standardized_time))
+            self.__epsilon=1.1-(1/cosh+(self.__num_epoch*C/self.__max_epoch))
     
     def decay_learning_rate(self) -> None:
         if self.__learning_rate <= self.__learning_rate_min:
             return
-        self.__learning_rate *= 0.9999999
+        self.__learning_rate *= 0.999999
     
     def run_training(self,  num_of_episode: int):
         """Overrides base class method
 
         Args:
-            num_of_episode (int): Number of episdoe to run
+            num_of_episode (int): Number of episode to run
         """
+        self.__is_production = False
         cumulated_reward = 0
         for _ in range(num_of_episode):
-            cumulated_reward += self.run_single_episode()
+            cumulated_reward += self.run_single_episode_training()
         
         print(f"Epsilon: {self.__epsilon}, Discounted reward: {self.__discounted_reward}, Learning rate: {self.__learning_rate}")
         print(f"Mean reward is: {cumulated_reward/num_of_episode} for {num_of_episode} episodes")
-        self.save_pickle("QL_parameters.pkl")
+        if (self.__to_save_pickle):
+            self.save_pickle("QL_parameters.pkl")
+    
+    def run_training_for_plot(self,  num_of_episode: int) -> float:
+        cumulated_reward = 0
+        for _ in range(num_of_episode):
+            cumulated_reward += self.run_single_episode_training()
+        return cumulated_reward/num_of_episode
     
     def run_production(self, num_of_episode: int):
         self.__is_production = True
@@ -216,7 +232,8 @@ class QLearningAgent(RLAgent):
             s_prime = self.discretise_observation(s_prime)
             
             self.update_q_table(s,R,s_prime)
-            self.update_parameters()
+            
+        self.update_parameters()
         return self._total_reward
     
     def run_single_episode_production(self) -> int:
@@ -234,10 +251,35 @@ class QLearningAgent(RLAgent):
             s_prime = self.discretise_observation(s_prime)
         return self._total_reward
 
+    def get_optimal_action(self, s: Observation) -> int:
+        """Gets optimal action for given state
+
+        Args:
+            s (Observation): State observed
+
+        Returns:
+            int: action to take, subjected to epsilon soft policy
+        """
+        # a* is the policy from the pi table
+        if (self.__is_production):
+            a_star: int = self.get_policy(s)
+            return a_star
+        
+        epsilon_over_A: float = self.__epsilon / len(self.__actions)
+        
+        # apply epsilon soft policy here to encourage exploration
+        if (np.random.randn() < 1 - self.__epsilon + epsilon_over_A):
+            # pick optimal
+            self.__pi_table[s] = self.argmax_a_Q(s,self.__actions)
+        else:
+            # pick random
+            self.__pi_table[s] = self.get_random_action()
+        return self.__pi_table[s]
+    
     def update_q_table(self,s: Observation, R: float, s_prime: Observation):
-        Q_S_A = self.__q_table[(s,self.__pi_table[s])]
+        Q_S_A = self.Q(s,self.__pi_table[s])
         Q_S_A = Q_S_A + self.__learning_rate * \
-                (R + self.__discounted_reward*self.Q(s_prime, self.argmax_a_Q(s_prime,self.__actions)) - Q_S_A)
+                (R + self.__discounted_reward*self.max_Q(s_prime,self.__actions) - Q_S_A)
         
         self.__q_table[(s,self.__pi_table[s])] = Q_S_A
 
@@ -248,6 +290,18 @@ class QLearningAgent(RLAgent):
             self.__q_table[(state,action)] = 0
             return 0
     
+    def max_Q(self, state: Observation, action_set: List[int]) -> float:
+        """Gets the max value of Q over all actions
+
+        Args:
+            state (Observation): state observed
+            action_set (List[int]): list of possible actions
+
+        Returns:
+            float: max value of Q for state
+        """
+        return max([self.Q(state,action) for action in action_set])
+
     def argmax_a_Q(self, state: Observation, action_set: List[int]) -> int:
         """Returns action that maximises Q function
 
@@ -259,8 +313,14 @@ class QLearningAgent(RLAgent):
             int: action
         """
         return max([(action,self.Q(state,action)) for action in action_set],key=lambda item:item[1])[0]
-       
-    # todo: action set should be passed into the function for consistency 
+    
+    def get_policy(self, state: Observation):
+        if (state not in self.__pi_table):
+            self.__pi_table[state] = self.get_random_action()
+            for a in self.__actions: self.__q_table[(state,a)] = 0
+        
+        return self.__pi_table[state]
+    
     def get_random_action(self) -> int:
         """Randomly generates an action.
         
@@ -272,10 +332,7 @@ class QLearningAgent(RLAgent):
             return math.ceil(val)
         else:
             return round(val)
-    
-    def print_q_table(self):
-        print(self.__q_table)
-        
+ 
     def get_q_table(self):
         return self.__q_table
     
@@ -306,12 +363,13 @@ class QLearningAgent(RLAgent):
         with open(parameters_file, 'wb') as file:
             # Call load method to deserialze
             pickle.dump([self.__pi_table,self.__q_table,self.__epsilon,self.__learning_rate], file)
-        
-    
+
+
+ 
 import os
 if __name__ == "__main__":
     world = CartpoleWorld()
-    agent = QLearningAgent(world)
+    agent = QLearningAgent(world,to_load_pickle=False,to_save_pickle=False,epsilon_decay=1)
     # print(sorted(agent.get_q_table().items(), key = lambda x : x[1]))
     # world.set_display_mode()
     # for i in range(100):
